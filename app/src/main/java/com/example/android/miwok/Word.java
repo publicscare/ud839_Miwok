cde992cef5adf4618f4b414ab333a67a45651be6
Brian Crouse <brian.crouse@outlook.com>
2020-03-10 07:08:41 -0500

    init

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,34 @@
+package com.example.android.miwok;
+
+public class Word {
+    private String mMiwokTranslation;
+    private String mDefaultTranslation;
+
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation is the word in the Miwok language
+     */
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    /**
+     * Get the default translation of the word.
+     */
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    /**
+     * Get the Miwok translation of the word.
+     */
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+
+}

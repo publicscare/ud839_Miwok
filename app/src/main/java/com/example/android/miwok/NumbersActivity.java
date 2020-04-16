252b90422cde1a47f2c63b930d71ebbabb480f31
Brian Crouse <brian.crouse@outlook.com>
2020-04-15 23:05:10 -0500

    Numbers and Colors refactored to Fragments

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,102 +1,16 @@
 package com.example.android.miwok;
 
-import android.media.MediaPlayer;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
+import android.os.Bundle;
 
 public class NumbersActivity extends AppCompatActivity {
-    private MediaPlayer mMediaPlayer;
-
-    /**
-     * Clean up the media player by releasing its resources.
-     */
-    private void releaseMediaPlayer() {
-        // If the media player is not null, then it may be currently playing a sound.
-        if (mMediaPlayer != null) {
-            // Regardless of the current state of the media player, release its resources
-            // because we no longer need it.
-            mMediaPlayer.release();
-
-            // Set the media player back to null. For our code, we've decided that
-            // setting the media player to null is an easy way to tell that the media player
-            // is not configured to play an audio file at the moment.
-            mMediaPlayer = null;
-        }
-    }
-
-    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
-        @Override
-        public void onCompletion(MediaPlayer mediaPlayer) {
-            // Now that the sound file has finished playing, release the media player resources.
-            releaseMediaPlayer();
-        }
-    };
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        // When the activity is stopped, release the media player resources because we won't
-        // be playing any more sounds.
-        releaseMediaPlayer();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        // When the activity is stopped, release the media player resources because we won't
-        // be playing any more sounds.
-        releaseMediaPlayer();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.word_list);
-
-        // Create a list of words
-        final ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("one", "lutti", R.mipmap.number_one, R.raw.number_one));
-        words.add(new Word("two", "otiiko", R.mipmap.number_two, R.raw.number_two));
-        words.add(new Word("three", "tolookosu", R.mipmap.number_three, R.raw.number_three));
-        words.add(new Word("four", "oyyisa", R.mipmap.number_four, R.raw.number_four));
-        words.add(new Word("five", "massokka", R.mipmap.number_five, R.raw.number_five));
-        words.add(new Word("six", "temmokka", R.mipmap.number_six, R.raw.number_six));
-        words.add(new Word("seven", "kenekaku", R.mipmap.number_seven, R.raw.number_seven));
-        words.add(new Word("eight", "kawinta", R.mipmap.number_eight, R.raw.number_eight));
-        words.add(new Word("nine", "wo’e", R.mipmap.number_nine, R.raw.number_nine));
-        words.add(new Word("ten", "na’aacha", R.mipmap.number_ten, R.raw.number_ten));
-
-        // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
-        // adapter knows how to create list items for each item in the list.
-        final WordAdapter adapter = new WordAdapter(this, words, R.color.category_numbers);
-
-        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
-        // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // activity_numbers.xml layout file.
-        ListView listView = (ListView) findViewById(R.id.list);
-
-
-        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
-        // {@link ListView} will display list items for each {@link Word} in the list.
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                //Toast.makeText(NumbersActivity.this, "List item clicked", Toast.LENGTH_SHORT);
-                releaseMediaPlayer();
-                Word word = words.get(position);
-                mMediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getAudioResourceId());
-                mMediaPlayer.start();
-                mMediaPlayer.setOnCompletionListener(mCompletionListener);
-            }
-        });
+        setContentView(R.layout.activity_category);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new NumbersFragment())
+                .commit();
     }
-
 }
